26a0a9e4f5c981b376146e360c6b8e6e12d8ab15
Oliver Reyes <oliverreyes.a@gmail.com>
2018-08-01 22:31:38 -0700

    [HRLY-4] Implemented deadline picker

--- TaskListView/TaskView/TaskView.js
+++ TaskListView/TaskView/TaskView.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, TouchableHighlight, TextInput, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableHighlight, TextInput, TouchableOpacity, DatePickerIOS } from 'react-native';
 
 /**
   * Presentational component
@@ -12,12 +12,12 @@ export default class TaskView extends React.Component {
     //const {params} = this.props.navigation.state;
     return (
       <View style={styles.container}>
-        <View style={styles.input_container}>
+        <View style={styles.title_container}>
           <TouchableOpacity
             style={styles.input_button}
             onPress={this.props._toggleTextInput}>
-            { this.props.show_text_input ? <TextInput
-              style={styles.input_header}
+            { this.props.show_input ? <TextInput
+              style={[styles.header,styles.input_header]}
               onChangeText={this.props._changeTitle}
               value={this.props.title}
               onSubmitEditing={this.props._toggleTextInput}
@@ -25,24 +25,29 @@ export default class TaskView extends React.Component {
             }
           </TouchableOpacity>
         </View>
-        <View style={styles.input_container}>
+        <View style={styles.row_container}>
+          <Text style={styles.text}>Deadline</Text>
           <TouchableOpacity
-            style={styles.input_button}
-            onPress={this.props._toggleTextInput}>
-            { this.props.show_text_input ? <TextInput
-              style={styles.input_text}
-              onChangeText={this.props._changeDl}
-              value={this.props.dl}
-              onSubmitEditing={this.props._toggleTextInput}
-              /> : <Text style={styles.text}>Deadline - {this.props.dl}</Text>
+            onPress={this.props._togglePicker}>
+            {
+              this.props.show_picker ? <Text style={styles.text}>Cancel</Text> :
+              <Text style={styles.text}>{this.props.dl}</Text>
             }
           </TouchableOpacity>
         </View>
-        <View style={styles.input_container}>
+        {
+          this.props.show_picker ? <DatePickerIOS
+          onDateChange={this.props._setDeadline}
+          date={this.props.new_dl}
+          mode='date'
+          minimumDate={this.props.today} /> : null
+        }
+        <View style={styles.row_container}>
           <TouchableOpacity
             style={styles.input_button}
             onPress={this.props._toggleTextInput}>
-            { this.props.show_text_input ? <TextInput
+            {
+              this.props.show_input ? <TextInput
               style={styles.input_text}
               //onChangeText={this.props._changeNotif}
               value={this.props.notif ? "On" : "Off"}
@@ -51,11 +56,11 @@ export default class TaskView extends React.Component {
             }
             </TouchableOpacity>
         </View>
-        <View style={styles.input_container}>
+        <View style={styles.row_container}>
           <TouchableOpacity
             style={styles.input_button}
             onPress={this.props._toggleTextInput}>
-            { this.props.show_text_input ? <TextInput
+            { this.props.show_input ? <TextInput
               style={styles.input_text}
               //onChangeText={this.props._changeRepeat}
               value={"5"}
@@ -64,7 +69,8 @@ export default class TaskView extends React.Component {
             }
           </TouchableOpacity>
         </View>
-        { !this.props.show_text_input && this.props.modified ? <TouchableOpacity
+
+        { !this.props.show_input && this.props.modified ? <TouchableOpacity
           style={styles.input_button}
           onPress={this.props._editTask}>
           <Text>Save</Text>
@@ -81,7 +87,7 @@ export default class TaskView extends React.Component {
   <TouchableOpacity
     style={styles.input_button}
     onPress={this.props._toggleTextInput}>
-    { this.props.show_text_input ? <TextInput
+    { this.props.show_input ? <TextInput
       style={styles.input_text}
       onChangeText={this.props._changeStatus}
       value={this.props.status}
@@ -95,39 +101,53 @@ export default class TaskView extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'flex-start',
+    alignItems: 'stretch',
     justifyContent: 'flex-start',
-    backgroundColor: 'powderblue',
-    alignSelf: 'stretch'
+    backgroundColor: '#FFB000',
+    //alignSelf: 'stretch'
+  },
+  row_container: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    //borderBottomColor: '#BDBDBD',
+    //borderBottomWidth: 0.3,
+    shadowOffset: { width: 0, height: -0.1 },
+    shadowColor: 'black',
+    shadowOpacity: 0.25,
+    backgroundColor: '#FFB000',
+
   },
   header: {
-    fontSize: 50,
+    fontSize: 30,
     fontWeight: 'bold',
+    marginLeft: 10,
+    marginRight: 10,
+    //color: '#212121',
+    color: '#FFFFFF'
   },
-  task_options: {
-    alignSelf: 'stretch',
-    borderBottomWidth: 2,
-    borderColor: 'grey',
-    marginVertical: 20
+  input_header: {
+    overflow: 'scroll',
+    //backgroundColor: 'rgb(0,0,0,0.3)'
   },
   text: {
-    fontSize: 30,
-    marginBottom: 5
+    fontSize: 18,
+    marginLeft: 10,
+    marginRight: 10,
+    color: '#FFFFFF'
+  },
+  title_container: {
+    flex: 3,
+    paddingVertical: 10,
   },
   input_container: {
     alignSelf: 'stretch',
     paddingVertical: 10,
     height: 100
   },
-  input_header: {
-    fontSize: 50,
-    fontWeight: 'bold',
-    marginVertical: 10,
-    overflow: 'scroll'
-  },
+
   input_text: {
-    fontSize: 30,
-    marginBottom: 5,
     overflow: 'scroll'
   }
 });
--- TaskListView/TaskView/TaskViewContainer.js
+++ TaskListView/TaskView/TaskViewContainer.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import DatePickerIOS from 'react-native';
 import { connect } from 'react-redux';
 import TaskView from './TaskView';
 import { editTask } from '../../redux/actions/actions';
+import moment from 'moment';
 
 /**
   * Container component
@@ -17,16 +19,20 @@ class TaskViewContainer extends Component {
       dl: "",
       notif: "",
       exp: "",
-      //completed: "",
-      show_text_input: false,
-      modified: false
+      show_input: false,
+      show_picker: false,
+      modified: false,
+      new_dl: new Date(),
+      today: ''
     };
     this._toggleTextInput = this._toggleTextInput.bind(this);
+    this._togglePicker = this._togglePicker.bind(this);
     this._editTask = this._editTask.bind(this);
     this._changeTitle = this._changeTitle.bind(this);
-    this._changeDl = this._changeDl.bind(this);
+    //this._changeDl = this._changeDl.bind(this);
     this._changeNotif = this._changeNotif.bind(this);
     this._changeExp = this._changeExp.bind(this);
+    this._setDeadline = this._setDeadline.bind(this);
     //this._changeCompleted = this._changeCompleted.bind(this);
   }
 
@@ -34,34 +40,44 @@ class TaskViewContainer extends Component {
     //const this_task = this.props.tasks.tasks.task_list.find(x => x.id === this.props.navigation.state.params.id)
     //console.log(this_task);
     const {data} = this.props.navigation.state.params;
+    let deadline = data.deadline;
+    console.log(deadline);
+    if (data.deadline === null){
+      deadline = 'None';
+    }
+    else {
+      deadline = deadline.toString().slice(0,10);
+      deadline = moment(deadline,'YYYY-MM-DD').format('ddd MMM DD YYYY');
+    }
+    console.log(deadline);
 
     // On mount, find object in redux array using passed id and update component state with data
     this.setState({
       id: data.id,
       title: data.title,
-      dl: data.deadline,
+      dl: deadline,
       notif: data.notifications,
       exp: data.exp,
-      completed: data.completed
+      today: new Date()
     });
   }
 
   _toggleTextInput(){
     this.setState(prevState => ({
-      show_text_input: !prevState.show_text_input
+      show_input: !prevState.show_input
+    }));
+  }
+
+  _togglePicker(){
+    console.log("SHOW PICKER " + this.state.show_picker);
+    this.setState(prevState => ({
+      show_picker: !prevState.show_picker
     }));
   }
 
   _changeTitle(new_input){
       this.setState({
         title: new_input,
-        modified: true
-      });
-  }
-
-  _changeDl(new_input){
-      this.setState({
-        dl: new_input,
         modified: true
       });
   }
@@ -87,8 +103,17 @@ class TaskViewContainer extends Component {
       });
   }
   */
+  _setDeadline(newDate){
+    console.log(newDate);
+    this.setState({
+      new_dl: newDate,
+      modified: true,
+      //show_picker: false
+    });
+  }
+
   _editTask(){
-    const data = {
+    let data = {
       id: this.state.id,
       title: this.state.title,
       deadline: this.state.dl,
@@ -96,6 +121,17 @@ class TaskViewContainer extends Component {
       exp: this.state.exp,
       //completed: this.state.completed
     };
+    if (this.state.show_picker){
+      const stringDate = this.state.new_dl.toString().substr(0,15);
+      console.log("STRING DATE: " + stringDate);
+      data = { ...data,
+        deadline: this.state.new_dl
+      };
+      this.setState({
+        dl: stringDate,
+        show_picker: false
+      });
+    }
     this.props.editTask(data);
     this.setState({
       modified: false
@@ -106,18 +142,22 @@ class TaskViewContainer extends Component {
     return <TaskView /*{...this.props}*/
     _editTask={this._editTask}
     _toggleTextInput={() => this._toggleTextInput()}
-    show_text_input={this.state.show_text_input}
+    show_input={this.state.show_input}
+    _togglePicker={() => this._togglePicker()}
+    show_picker={this.state.show_picker}
     _changeTitle={(title) => this._changeTitle(title)}
-    _changeDl={(dl) => this._changeDl(dl)}
+    _setDeadline={(new_dl) => this._setDeadline(new_dl)}
     _changeNotif={(notif) => this._changeNotif(notif)}
     _changeExp={(exp) => this._changeExp(exp)}
     //_changeCompleted={(status) => this._changeCompleted(status)}
     title={this.state.title}
     dl={this.state.dl}
+    new_dl={this.state.new_dl}
     exp={this.state.exp}
     notif={this.state.notif}
     //completed={this.state.completed}
-    modified={this.state.modified} />;
+    modified={this.state.modified}
+    today={this.state.today} />;
   }
 }
 
